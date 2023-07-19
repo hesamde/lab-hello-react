7fbb848a1cc043c27976787b77e065c70b5da9e3
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-07-19 13:01:48 +0200

    -Iteration-4-design-lab-done

---
+++ hello-react-project/src/components/Logo.js
@@ -0,0 +1,11 @@
+import ironhackLogo from "../images/ironhack-logo-xs.png";
+
+const Logo = () => {
+  return (
+    <div className="navbar-brand">
+      <img src={ironhackLogo} className="app-logo" alt="app-logo" width="50" />
+    </div>
+  );
+};
+
+export default Logo;
